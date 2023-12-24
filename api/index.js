0efc1783ea9f70efe82afb6b7dc2da31f83e8f02
chuanzheng55 <100614122+chuanzheng55@users.noreply.github.com>
2023-12-23 22:17:26 -0500

    test api route

--- api/index.js
+++ api/index.js
@@ -1,20 +1,30 @@
-import express from "express";
+import express, { request, response } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
 mongoose
-.connect(process.env.MONGO)
-.then(() => {
-  console.log("Connected to MongoDB!");
-})
-.catch((err) => {
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
+// req from client, res from server
+app.get("/test", (req, res) => {
+  res.json({
+    message: "HJKJAKSEHJ",
+  });
+});
+
+app.use("/api/user", userRouter);
