15a93ba47f3720c681ea486074ad758169cb622c
chuanzheng55 <100614122+chuanzheng55@users.noreply.github.com>
2023-12-30 01:59:18 -0500

    created user update api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,36 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
   res.json({
     message: "user controller",
   });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id != req.params.id)
+    return next(errorHandler(401, "Log in your own account to update"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    ); // new information instead of previous
+
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
