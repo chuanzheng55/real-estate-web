134af3296dd83cd6034fb098682fee4d38ccd4b2
chuanzheng55 <100614122+chuanzheng55@users.noreply.github.com>
2024-01-01 23:30:43 -0500

    added listing api route

---
+++ api/models/listing.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    discountPrice: {
+      type: Number,
+      require: true,
+    },
+    bedrooms: {
+      type: Number,
+      require: true,
+    },
+    bathrooms: {
+      type: Number,
+      require: true,
+    },
+    furnished: {
+      type: Boolean,
+      require: true,
+    },
+    parking: {
+      type: Boolean,
+      require: true,
+    },
+    type: {
+      type: String,
+      require: true,
+    },
+    offer: {
+      type: Boolean,
+      require: true,
+    },
+    ImageUrls: {
+      type: Array,
+      require: true,
+    },
+    userRef: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+export default Listing;
