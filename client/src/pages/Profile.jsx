5039e4f0c3abefe39cdd24237dbf131266d4e3cc
chuanzheng55 <100614122+chuanzheng55@users.noreply.github.com>
2024-01-04 00:29:55 -0500

    added delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -143,6 +143,25 @@ const Profile = () => {
     }
   };
 
+  const handleDeleteListing = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -225,43 +244,55 @@ const Profile = () => {
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated successfully!" : " "}
       </p>
-      <button onClick={handleShowListings} className=" text-green-700 w-full p-3 border rounded-lg hover:opacity-65">
+      <button
+        onClick={handleShowListings}
+        className=" text-green-700 w-full p-3 border rounded-lg hover:opacity-65"
+      >
         Show Listings
       </button>
       <p className="text-red-700 mt-5">
         {showListingsError ? "Error showing the listings" : " "}
       </p>
-      
-      {userListings &&
-        userListings.length > 0 &&
+
+      {userListings && userListings.length > 0 && (
         <div className="flex flex-col gap-4">
-          <h1 className="text-center mt-7 text-3xl font-semibold">Your Listings</h1>
-        {userListings.map((listing) => (
-          <div
-            key={listing._id}
-            className="border border-slate-400 rounded-lg p-3 flex justify-between items-center gap-5"
-          >
-            <Link to={`/listing/${listing._id}`}>
-              <img
-                src={listing.imageUrls[0]}
-                alt="listing cover "
-                className="h-16 w-16 object-contain"
-              ></img>
-            </Link>
-            <Link
-              className="text-slate-700 font-semibold flex-1 hover:underline truncate"
-              to={`/listing/${listing._id}`}
+          <h1 className="text-center mt-7 text-3xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border border-slate-400 rounded-lg p-3 flex justify-between items-center gap-5"
             >
-              <p>{listing.name}</p>
-            </Link>
-            <div className="flex flex-col p-3 items-center">
-            <button type="button" className="text-green-700 uppercase">Edit</button>
-              <button type="button" className="text-red-700 uppercase" >Delete</button>
-              
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover "
+                  className="h-16 w-16 object-contain"
+                ></img>
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold flex-1 hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col p-3 items-center">
+                <button type="button" className="text-green-700 uppercase">
+                  Edit
+                </button>
+                <button
+                  onClick={() => handleDeleteListing(listing._id)}
+                  type="button"
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
-        </div>}
+          ))}
+        </div>
+      )}
     </div>
   );
 };
