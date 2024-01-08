a535b26a1e77cabe4c0ace5f8537d98d194856fa
chuanzheng55 <100614122+chuanzheng55@users.noreply.github.com>
2024-01-08 14:20:56 -0500

    fixed mobile view sign in button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -58,15 +58,15 @@ const Header = () => {
           <Link to="/about">
             <li className="hidden sm:inline hover:underline ">About</li>
           </Link>
-          <Link to="/profile">
+          <Link to='/profile'>
             {currentUser ? (
               <img
-                className="rounded-full h-7 w-7 object-cover"
+                className='rounded-full h-7 w-7 object-cover'
                 src={currentUser.avatar}
-                alt="profile"
+                alt='profile'
               />
             ) : (
-              <li className="hidden sm:inline hover:underline ">Sign in</li>
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
             )}
           </Link>
         </ul>
